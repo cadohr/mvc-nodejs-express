be81d3038595d3682b8840b6e1aeb6790417d033
Ricardo Jacinto <cadohr@gmail.com>
2015-07-28 21:22:12 -0300

    second commit

--- controllers/message.js
+++ controllers/message.js
@@ -1,21 +1,31 @@
-var mongoose = require('mongoose')
-var Video = require('../models/message');
+var fs = require('fs')
+    ,xml2js = require('xml2js')
+    ,parser = new xml2js.Parser()
+    ,mongoose = require('mongoose');
+
 module.exports.controller = function(router) {
+  router.get('/', function(req, res) {
+    res.render('message/message')
+  });
 
-/**
- * a home page route
- */
   router.get('/message', function(req, res) {
-      // any logic goes here
-      res.render('message/message')
-  });
+    var s2 = fs.readFile('./msg/messagens.xml', 'utf8', function(err,data) {
+      if (err) {
+        return console.log('readFile error: ' + err);
+      }
 
-/**
- * About page route
- */
-  router.get('/jean', function(req, res) {
-      // any logic goes here
-      res.render('message/jean')
-  });
+      var json = parser.parseString(data, function(err,result){
+        if (err) {
+          return console.log('parseString error: ' + err);
+        }
+        //Extract the value from the data element
+        var obj = JSON.stringify(result);
 
+        return obj;
+      });
+    });
+
+    res.render('message/message');
+    res.json(s2);
+  });
 }
